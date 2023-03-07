9e776c5d47ac3963e25b55bd97a7ab04dfa94e34
Edward <edward.klinghammer@gmail.com>
2023-03-07 09:35:23 +0100

    json och bort med solver1

--- solver2.js
+++ solver2.js
@@ -195,3 +195,44 @@ async function solve2(grid) {
     showPv(grid)
     return grid
 }
+function isPossibleMove(grid, y, x, number) {
+    let check = []
+    // Vertical And Horizontal
+    for (let i = 0; i < grid.length; i++) {
+        if (i !== y) { check.push([i, x]) }
+        if (i !== x) { check.push([y, i])}
+    }
+    // 3x3 Box
+    let left = 3 * Math.floor(x / 3)
+    let top = 3 * Math.floor(y / 3)
+    for (let i = top; i < top + 3; i++) {
+        for (let j = left; j < left + 3; j++) {
+            if (grid[i][j].value != 0 && (i !== y && j !== x)) { check.push([i, j]) }
+        }
+    }
+    
+    for (let cell of check) {
+        let y2 = cell[0]
+        let x2 = cell[1]
+        if (grid[y2][x2].value == number) {
+            return {x:x2,y:y2}
+        }
+    }
+    return true
+}
+function resetValues(grid) { for (let y = 0; y < grid.length; y++) { for (let x = 0; x < grid[y].length; x++) { grid[y][x].possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9] }}}
+
+
+function copyProperty(arr, prop) {
+    let temp = []
+    for (let i = 0; i < arr.length; i++) {
+        temp.push([])
+        for (let j = 0; j < arr[i].length; j++) {
+            if (Array.isArray(arr[i][j][prop])) {
+                temp[i].push([])
+                for (let e of arr[i][j][prop]) { temp[i][j].push(e) }
+            } else { temp[i].push(arr[i][j][prop]) }
+        }
+    }
+    return temp
+}
